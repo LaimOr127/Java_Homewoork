ddefb47d4f9b683b624cf65e88d6d94bc58fd541
Alex <95967934+LaimOr127@users.noreply.github.com>
2023-10-10 10:37:34 +0300

    Update WaitList.java

--- src/Homework/Pr14/WaitList.java
+++ src/Homework/Pr14/WaitList.java
@@ -6,13 +6,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-/**
- * Base class WaitList with base functionality and minimum needed field - {@link WaitList#components} for storing data.
- * @param <E> Type of stored data.
- * @see IWaitList
- * @author aleksandrgracev
- * @version 1.0
- */
+
 public class WaitList <E> implements IWaitList<E> {
     /**
      * Field for storing data. FIFO queue from standard library.
